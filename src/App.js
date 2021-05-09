4ec9d38d4d151ba91740c1e2c044d7060fe99049
Ivan Torres <ivantorresmia27@gmail.com>
2021-05-08 20:01:29 -0700

    reformated code to have js file for images

--- src/App.js
+++ src/App.js
@@ -2,23 +2,19 @@ import React, { Component, useState } from "react";
 import NavBar from "./components/Navbar";
 import Jumbo from "./components/Jumbo";
 import Wrapper from "./components/Wrapper";
-import Characters from "./Characters.json";
+import Characters from "./Characters.js";
 import Card from "./components/Card";
 
 let hasBeenClicked = [];
 
-
 class App extends Component {
   state = {
-    // THIS IS STORING ALL MY JSON FILE 
+    // THIS IS STORING ALL MY JSON FILE
     Characters: Characters,
     score: 0,
     highScore: 0,
-    message: "Click a picture to start"
+    message: "Click a picture to start",
   };
-
-
-
 
   handleClick = (event) => {
     console.log(event.target);
@@ -26,60 +22,56 @@ class App extends Component {
 
     console.log(hasBeenClicked);
 
-this.setState({message: "Let's play"})
+    this.setState({ message: "Let's play" });
 
-if (!hasBeenClicked.includes(currentId)) {
-  hasBeenClicked.push(currentId)
-  const newCount = this.state.score + 1;
-  // const highScoreCount = this.state.highScore +1;
-  this.setState({score: newCount});
-  this.setState({message: "Nice!"})
-  if (newCount > this.state.highScore) {
-    this.setState({highScore: newCount})
-  }
-} else {
-  this.setState({score: 0});
-  this.setState({message: "Wrong Buddy!"})
-  hasBeenClicked = [];
-
-
-}
-
-      // Shuffles the array of characters
-      const arr = this.state.Characters;
-      for (let i = arr.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * arr.length);
-        let temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+    if (!hasBeenClicked.includes(currentId)) {
+      hasBeenClicked.push(currentId);
+      const newCount = this.state.score + 1;
+      // const highScoreCount = this.state.highScore +1;
+      this.setState({ score: newCount });
+      this.setState({ message: "Nice!" });
+      if (newCount > this.state.highScore) {
+        this.setState({ highScore: newCount });
       }
-      this.setState({ Characters: arr });
+    } else {
+      this.setState({ score: 0 });
+      this.setState({ message: "Wrong Buddy!" });
+      hasBeenClicked = [];
+    }
+
+    // Shuffles the array of characters
+    const arr = this.state.Characters;
+    for (let i = arr.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * arr.length);
+      let temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+    this.setState({ Characters: arr });
   };
 
   render() {
     return (
       <Wrapper>
         <NavBar
-        score = {this.state.score}
-        topScore ={this.state.highScore}
-        message={this.state.message}
+          score={this.state.score}
+          topScore={this.state.highScore}
+          message={this.state.message}
         />
         <Jumbo />
         <div className="container">
           <div className="row">
-        {this.state.Characters.map((character) => (
-          <Card
-            key={character.id}
-            imageURL={character.image}
-            id={character.id}
-            alt={character.name}
-            handleClick={this.handleClick}
-         
-          />
-          
-        ))}
-</div>
-</div>
+            {this.state.Characters.map((character) => (
+              <Card
+                key={character.id}
+                imageURL={character.image}
+                id={character.id}
+                alt={character.name}
+                handleClick={this.handleClick}
+              />
+            ))}
+          </div>
+        </div>
       </Wrapper>
     );
   }
--- src/Characters.js
+++ src/Characters.js
@@ -1,74 +1,91 @@
-[
+import shrek from './Images/shrek1.jpg';
+import cat from './Images/cat.jpg';
+import babyshrek from './Images/babyshrek.jpg';
+import cookieman from "./Images/cookie-man.jpg";
+import donkey from "./Images/donkey.jpg";
+import fiona from  "./Images/fiona.jpg"
+import ogarfionna from "./Images/ogarFione.jpg";
+import pigs from "./Images/pigs.jpg";
+import king from "./Images/king.jpg";
+import lord from "./Images/Lord-f.jpg";
+import shrekhuman from "./Images/ShrekHuman.jpg";
+import fairy from "./Images/fairy.jpg";
+
+
+
+const Characters = [
     {
         "id": 1,
         "name": "shrek",
-        "image": "./assets/shrek1.jpg",
+        "image": shrek,
         "clicked": false
     },
     {
         "id": 2,
         "name": "cat",
-        "image": "./assets/cat.jpg",
+        "image": cat,
         "clicked": false
     },
     {
         "id": 3,
         "name": "babyshrek",
-        "image": "./assets/babyshrek.jpg",
+        "image": babyshrek,
         "clicked": false
     },
     {
         "id": 4,
         "name": "cookieman",
-        "image": "./assets/cookie-man.jpg",
+        "image": cookieman,
         "clicked": false
     },
     {
         "id": 5,
         "name": "donkey",
-        "image": "./assets/donkey.jpg",
+        "image": donkey,
         "clicked": false
     },
     {
         "id": 6,
         "name": "Fiona",
-        "image": "./assets/fiona.jpg",
+        "image": fiona,
         "clicked": false
     },
     {
         "id": 7,
         "name": "OgarFiona",
-        "image": "./assets/ogarFione.jpg",
+        "image": ogarfionna,
         "clicked": false
     },
     {
         "id": 8,
         "name": "Pigs",
-        "image": "./assets/pigs.jpg",
+        "image": pigs,
         "clicked": false
     },
     {
         "id": 9,
         "name": "King",
-        "image": "./assets/king.jpg",
+        "image": king,
         "clicked": false
     },
     {
         "id": 10,
         "name": "LordF",
-        "image": "./assets/Lord-f.jpg",
+        "image": lord,
         "clicked": false
     },
     {
         "id": 11,
         "name": "ShrekHuman",
-        "image": "./assets/ShrekHuman.jpg",
+        "image": shrekhuman,
         "clicked": false
     },
     {
         "id": 12,
         "name": "fairy",
-        "image": "./assets/fairy.jpg",
+        "image": fairy,
         "clicked": false
     }
 ]
+
+export default Characters;
