3d7f847aac54a9ae7d1204b78b5565f61d2a57b6
Ivan Torres <ivantorresmia27@gmail.com>
2021-05-09 17:39:34 -0700

    fixing navbar positioning

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,13 +1,9 @@
 import React from "react";
 
-function NavBar(props) {
+function NavBar() {
   return (
     <nav className="navbar">
-      <h1>Shrek Clicky</h1>
-      <h3 className="message">{props.message}</h3>
-      <h3>
-        Score: {props.score} | Top Score: {props.topScore}
-      </h3>
+      <h1>Shrek Memory Game</h1>
     </nav>
   );
 }
